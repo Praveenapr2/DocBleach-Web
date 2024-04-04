7c0a5cac6ef0d28dccd918bae618bd566a0ddd2e
Praveena <veenaraj9115@gmail.com>
2024-04-04 09:57:09 +0530

    commit

--- worker/docbleach/celeryconfig.py
+++ worker/docbleach/celeryconfig.py
@@ -1,3 +1,6 @@
 # Same TTL as Plik
 result_expires = 12 * 3600
 task_reject_on_worker_lost = True
+# celeryconfig.py
+broker_url = 'redis://localhost:6379/0'
+result_backend = 'redis://localhost:6379/0'
