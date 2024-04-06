c66392597ecf1309a616883054b7b3e7cf141931
Praveena <veenaraj9115@gmail.com>
2024-04-07 00:46:50 +0530

    commit

--- worker/docbleach/celeryconfig.py
+++ worker/docbleach/celeryconfig.py
@@ -2,7 +2,7 @@
 result_expires = 12 * 3600
 task_reject_on_worker_lost = True
 # celeryconfig.py
-result_backend = 'redis://172.19.0.2:6379/0'
+CELERY_RESULT_BACKEND  = 'redis://redis/'
 CELERY_DEFAULT_EXCHANGE = 'reply.celery.pidbox'
 CELERY_DEFAULT_ROUTING_KEY = '_kombu.binding.reply.celery.pidbox'
-CELERY_BROKER_URL = 'redis://172.19.0.2:6379/0'
+CELERY_BROKER_URL = 'redis://redis/'
